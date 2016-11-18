050a3992bb23b678d30726f6f622dcafb686da03
Lambert Zhang <tyouteis@gmail.com>
2016-11-18 10:50:38 +0900

    Load facebook sdk when need

--- client/imports/pages/topics/topic-detail.component.ts
+++ client/imports/pages/topics/topic-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams, NavController, PopoverController } from 'ionic-angular';
+import { NavParams, NavController, PopoverController, LoadingController } from 'ionic-angular';
 import { Meteor } from 'meteor/meteor';
 import { Topic } from '../../../../both/models/topic.model';
 import { Comment } from '../../../../both/models/comment.model';
@@ -11,7 +11,6 @@ import * as style from './topic-detail.component.scss';
 import { TopicOptionsComponent } from './topic-options.component';
 import { MeteorObservable } from 'meteor-rxjs';
 import { CommentsPage } from '../../pages/topics/comments-page.component';
-import { SocialSharing } from 'ionic-native';
 import { UtilityService } from '../../services/utility.service';
  
 @Component({
@@ -31,6 +30,7 @@ export class TopicDetail implements OnInit {
     navParams: NavParams,
     private navCtrl: NavController,
     private popoverCtrl: PopoverController,
+    private loadingCtrl: LoadingController,
     private utilSrv: UtilityService
   ) {
     this.topicId = navParams.get('topicId');
@@ -42,25 +42,6 @@ export class TopicDetail implements OnInit {
     this.topic.profile = user.profile;
     this.barTitle = this.utilSrv.editTitle(this.topic.title, 12);
     this.subComments();
-  }
-
-  ionViewDidEnter() {
-    let js;
-    let fjs = document.getElementsByTagName('script')[0];
-    if (document.getElementById('facebook-jssdk')) return;
-    js = document.createElement('script');
-    js.id = 'facebook-jssdk';
-    js.src = '//connect.facebook.net/ja_JP/sdk.js';
-    fjs.parentNode.insertBefore(js, fjs);
-
-    window.fbAsyncInit = () => {
-      FB.init({
-        appId      : '383813588676104',
-        status     : true,
-        xfbml      : true,
-        version    : 'v2.5'
-      });
-    };
   }
 
   showOptions(): void {
@@ -105,19 +86,23 @@ export class TopicDetail implements OnInit {
     return false;
   }
 
-  shareSocial() {
-    SocialSharing.share(this.topic.title, this.topic.title,  this.topic.picture,'http://192.168.11.11:3000/#/topic-detail/' + this.topic._id)
-    .then(()=>{
-    }).catch((e)=>{
-      console.log('e=', e);
-    })
-  }
-
   shareViaFacebook(): void {
-    FB.ui({
-      method: 'share',
-      href: 'http://www.ribenshier.com/#/topic-detail/' +  this.topic._id
-    }, (response) => {console.log('response=', response);});
+    this.initFB();
+
+    let loader = this.loadingCtrl.create({
+      content: '连接服务器...',
+      dismissOnPageChange: true
+    });
+
+    loader.present();
+
+    setTimeout(() => {
+      loader.dismissAll();
+      FB.ui({
+        method: 'share',
+        href: 'http://www.ribenshier.com/#/topic-detail/' +  this.topic._id
+      }, (response) => {console.log('fb response=', response);});
+    }, 3000);
   }
 
   private subComments(): void {
@@ -145,5 +130,24 @@ export class TopicDetail implements OnInit {
           ).zone();
       });
     });
+  }
+
+  private initFB() {
+    let js;
+    let fjs = document.getElementsByTagName('script')[0];
+    if (document.getElementById('facebook-jssdk')) return;
+    js = document.createElement('script');
+    js.id = 'facebook-jssdk';
+    js.src = '//connect.facebook.net/ja_JP/sdk.js';
+    fjs.parentNode.insertBefore(js, fjs);
+
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId      : '383813588676104',
+        status     : true,
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
   }
 }

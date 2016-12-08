4dc3140bcb79e0e09871a4dbebec4f768d24da94
haolin-liu <2665353751@qq.com>
2016-12-08 15:35:55 +0800

    New-topic changed

--- client/imports/pages/topics/new-topic.component.ts
+++ client/imports/pages/topics/new-topic.component.ts
@@ -10,6 +10,8 @@ import { Thumbs, Images } from '../../../../both/collections/images.collection';
 import { Thumb, Image } from '../../../../both/models/image.model';
 import { upload } from '../../../../both/methods/images.methods';
 import { UtilityService } from '../../services/utility.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
  
 @Component({
   selector: 'new-topic',
@@ -21,32 +23,47 @@ import { UtilityService } from '../../services/utility.service';
 export class NewTopicComponent {
   thumbs: Observable<Thumb[]>;
   private senderId: string;
-  private title: string;
-  private content: string;
+//private title: string;
+//private content: string;
   private picture: string;
   private pictureId: string;
   private thumbnail: string;
   private thumbId: string;
- 
+  private thumb: string;
+  newTopicForm: FormGroup;
+  title = new FormControl('',Validators.compose([
+                             Validators.required,
+                             Validators.minLength(1),
+                             Validators.maxLength(50)]));
+  content = new FormControl('',Validators.compose([
+                               Validators.required,
+                               Validators.minLength(1),
+                               Validators.maxLength(2000)]));;
   constructor(
-    private navCtrl: NavController, 
+    private navCtrl: NavController,
     private viewCtrl: ViewController,
     private loadingCtrl: LoadingController,
-    private utilSrv: UtilityService
+    private utilSrv: UtilityService,
+    private formBuilder: FormBuilder
   ) {
     this.senderId = Meteor.userId();
-  }
- 
+    this.newTopicForm = this.formBuilder.group({
+      title: this.title,
+      content: this.content,
+    });
+    }
+
   addTopic(): void {
-    MeteorObservable.call('addTopic', 
-                      this.senderId, 
-                      this.title, 
-                      this.content, 
-                      this.pictureId, 
+    MeteorObservable.call('addTopic',
+                      this.senderId,
+                      this.title.value,
+                      this.content.value,
+                      this.pictureId,
                       this.picture,
                       this.thumbId,
-                      this.thumbnail
-      ).subscribe({
+                      this.thumbnail,
+                      this.thumb
+       ).subscribe({
       next: () => {
         this.viewCtrl.dismiss();
       },
@@ -55,12 +72,48 @@ export class NewTopicComponent {
           this.utilSrv.alertDialog('发表失败', e.message);
         });
       }
-    }); 
+    });
   }
- 
-  uploadPicture(files): void {
+
+  uploadPicture(files,type): void {
     if(files.length == 0) {
       return;
+    }
+
+    if(files[0].type != 'image/jpeg'
+        && files[0].type !='image/jpg'
+        && files[0].type !='image/gif'
+        && files[0].type !='image/png'
+        && files[0].type !='image/bmp'
+        && files[0].type !='image/tiff'
+        && files[0].type !='image/pcx'
+        && files[0].type !='image/tga'
+        && files[0].type !='image/exif'
+        && files[0].type !='image/fpx'
+        && files[0].type !='image/svg'
+        && files[0].type !='image/psd'
+        && files[0].type !='image/pcd'
+        && files[0].type !='image/dxf'
+        && files[0].type !='image/eps'
+        && files[0].type !='image/ai'
+        && files[0].type !='image/raw'
+        && files[0].type !='image/ufo') {
+      this.utilSrv.alertDialog('图片格式不正确', '请重新选择图片上传');
+      return;
+    }
+
+     if (files[0].size > 512000) {
+      this.utilSrv.alertDialog('图片不能大于500KB', '请重新选择图片上传');
+      return;
+    }
+
+    if(type === 'main' && this.pictureId) {
+      Images.remove(this.pictureId);
+      Thumbs.remove(this.thumbId);
+      this.thumb = undefined;
+      this.thumbId = undefined;
+      this.picture = undefined;
+      this.pictureId = undefined;
     }
    
     let loader = this.loadingCtrl.create({
@@ -70,7 +123,7 @@ export class NewTopicComponent {
 
     loader.present();
     upload(files[0])
-      .then((result) => { 
+      .then((result) => {
         loader.dismissAll();
         this.picture = result.path;
         this.pictureId = result._id;
@@ -80,6 +133,22 @@ export class NewTopicComponent {
         this.utilSrv.alertDialog('图片上载失败', e.message);
       });
   }
+
+    removePicture(thumb: Thumb): void {
+      MeteorObservable.call('removePicture', thumb).subscribe({
+        next: () => {
+          if(thumb._id === this.thumbId) {
+          this.thumb = undefined;
+          this.thumbId = undefined;
+          this.picture = undefined;
+          this.pictureId = undefined;
+          }
+        },
+      error: (e: Error) => {
+        this.utilSrv.alertDialog('删除图片失败', e.message);
+      }
+      });
+    }
 
   private updatePicture(): void {
     MeteorObservable.autorun().subscribe(() => {

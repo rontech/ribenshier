aadffc515b5eba96921451a27513bc720b353fd3
Lambert Zhang <tyouteis@gmail.com>
2017-02-09 18:43:20 +0900

    Add files used by mobile

---
+++ ios_run.sh
@@ -0,0 +1,5 @@
+#MONGO_URL=mongodb://40.115.250.137:27017/meteor 
+# meteor run ios-de
+# meteor build ~/workspace/nodejs --server 40.115.250.137:80
+#meteor build ~/workspace/nodejs --server=http://www.ribenyan.net
+meteor build ~/dev/node.js/mobile --server=http://www.ribenshier.com
---
+++ mobile-config.js
@@ -0,0 +1,42 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'com.ribenshier',
+  name: '日本事儿',
+  description: '不同视角看日本',
+  author: 'Rontech',
+  email: 'dev@rontech.co.jp',
+  website: 'http://www.ribenshier.com'
+});
+
+// app 图标
+App.icons({
+  // 'iphone': 'appicons/iphone.png',
+  'iphone_2x': 'appicons/iphone_2x_120.png',
+  'iphone_3x': 'appicons/iphone_3x_180.png',
+  'ipad' : 'appicons/ipad_76.png',
+  'ipad_2x' :'appicons/ipad_2x_152.png',
+  'ipad_pro' : 'appicons/ipad_pro_167.png'
+});
+
+App.launchScreens({
+	'iphone_2x': 'launchScreen/iphone2x_640X960.png',
+	'iphone5': 'launchScreen/iphone5_640X1136.png',
+	'iphone6': 'launchScreen/iphone6_750X1334.png',
+	'iphone6p_portrait': 'launchscreen/iphone6s_1204X2208.png'
+});
+
+// 服务器
+App.accessRule('http://www.ribenshier.net/*');
+
+//wechat config 
+// 使用插件：boxfish:accounts-wechat
+// 插件地址：https://github.com/boxfish/meteor-accounts-wechat
+App.configurePlugin('cordova-plugin-wechat', {
+    WECHATAPPID: 'wxdd15b6922237eac5'
+});
+
+// // 添加包方法：meteor add cordova:xu.li.cordova.wechat@https://github.com/xu-li/cordova-plugin-wechat/tarball/ab08d76f322f7988e34231b3655300d3f36b37fa
+App.configurePlugin('cordova-plugin-wechat', {
+	WECHATAPPID: "wxdd15b6922237eac5"
+});
